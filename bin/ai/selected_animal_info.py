4d6d0e12d71621ce11cbc950d7aca17e2d82a29d
ATMCHGIT18 <torkamanchehaligit@gmail.com>
2026-09-11 10:46:01 +0200

    tools added for info gathering

--- bin/ai/selected_animal_info.py
+++ bin/ai/selected_animal_info.py
@@ -1,16 +1,123 @@
 import warnings
 warnings.filterwarnings('ignore')
 
+import random
+
 import os,sys
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
-from typing import List
-import json
-from pydantic import BaseModel,Field
+from bin.ai.animal_datastructure import *
+from bin.ai.animal_checker import get_search
+from bin.ai.GBIF_tools import gbif_taxonomy_data_grabber , gbif_geo_data_grabber
+import json 
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_agent
+from langgraph.graph import StateGraph,END,START
+from langchain.tools import tool
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def generate_random_hex():
+    # Generate a random integer up to 16777215 (0xFFFFFF) and convert to hex
+    return f"#{random.randint(0, 0xFFFFFF):06x}"
+
+def random_number():
+    return (random.randint(30,75) / 100)
+
+@tool("animal_data_creator",response_format='content')
+def animal_data_grabber(animal_page:WikipediaPage) -> AnimalInfoResult:
+    '''Searching articles in the wikipedia and the other sources that gives you the information
+    on the animal that is searched'''
+
+    animal_taxo_data , key = gbif_taxonomy_data_grabber(animal_page['scientific_name'])
+
+    geodata = gbif_geo_data_grabber(int(key))
+
+    distr_data = []
+
+    for i in geodata['features']:
+        props = AnimalDistributionProperties(
+            species=i['properties']['species'],
+            fill_color=generate_random_hex(),
+            fill_opacity=random_number(),
+            border_color=generate_random_hex(),
+            gbifID=i['properties']['gbifID'],
+            scientificName=i['properties']['scientificName'],
+            country=i['properties']['country'],
+            stateProvince=i['properties']['stateProvince'],
+            eventDate=i['properties']['eventDate'],
+            recordedBy=i['properties']['recordedBy'],
+            basisOfRecord=i['properties']['basisOfRecord'],
+            coordinateUncertaintyInMeters=i['properties']['coordinateUncertaintyInMeters'],
+            image=i['properties']['image'],
+            reference=i['properties']['reference']
+        )
+
+        geometry = AnimalDistributionGeometry(
+            type=i['geometry']['type'],
+            coordinate=i['geometry']["coordinates"]
+        )
+
+        data = AnimalDistributionFeature(
+            type="Feature",
+            properties=props,
+            geometry=geometry
+        )
+
+        distr_data.append(data)
 
 
-class AnimalInfoResult(BaseModel):
-    status:str = Field(description="Status of the animal. ")
+    distribution = AnimalDistributionFeatureCollection(
+        type="FeatureCollection",
+        data=distr_data
+
+    )
+
+
+    physical = AnimalPhysical()
+
+    conservation = AnimalConservation(
+        status="EN",
+        population_trend=""
+    )
+
+    ref = TaxonomyAnimalReference()
+
+    taxonomy = AnimalTaxonomy(
+        kingdom=animal_taxo_data['kingdom'],
+        phylum=animal_taxo_data['phylum'],
+        class_=animal_taxo_data['class'],
+        order=animal_taxo_data['order'],
+        family=animal_taxo_data['family'],
+        genus=animal_taxo_data['genus'],
+        species=animal_taxo_data['species'],
+        taxonomicStatus=animal_taxo_data['taxonomicStatus'],
+        reference=ref,
+        subspecies=None
+    )
+
+    result = AnimalInfoResult(
+        id=animal_taxo_data['scientificName'],
+        scientific_name=animal_taxo_data['canonicalName'],
+        common_name=animal_page['title'],
+        imageurl=animal_page['thumbnail']['url'],
+        taxonomy=taxonomy,
+        status=None,
+        clades=None,
+        synonyms=None,authority=animal_taxo_data['authorship'],
+        conservation=conservation,
+        physical=physical,
+        habitat=[""],
+        diet=[""],
+        distribution=distribution,
+        description=None
+    )
+
+    return result
+
+      
